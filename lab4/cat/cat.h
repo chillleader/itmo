ce8fd2ce0965c9cd45a678331a6af6fcfd11f0fb
Michael Gostev <gostevvm@yandex.ru>
2019-11-09 10:36:33 +0300

    Add first implementation of lab4

---
+++ lab4/cat/cat.h
@@ -0,0 +1,14 @@
+#ifndef OSP_LAB4_CAT_H
+#define OSP_LAB4_CAT_H
+
+int main(int argc, char *argv[]);
+
+void prepare_args(char **argv);
+
+void prepare_buf(FILE *);
+
+void raw_args(char *argv[]);
+
+void raw_cat(int);
+
+#endif //OSP_LAB4_CAT_H
