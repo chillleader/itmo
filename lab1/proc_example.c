a1dac9d25a430e60509db35a3efe8654f5bc0e3d
chillleader <easydraw20@gmail.com>
2021-02-20 17:34:53 +0300

    add example

--- lab1/proc_example.c
+++ lab1/proc_example.c
@@ -1,58 +1,91 @@
-#include <linux/init.h>
 #include <linux/module.h>
+#include <linux/version.h>
 #include <linux/kernel.h>
-#include <linux/proc_fs.h>
-#include <linux/string.h>
-#include <linux/uaccess.h>
+#include <linux/types.h>
+#include <linux/kdev_t.h>
+#include <linux/fs.h>
+#include <linux/device.h>
+#include <linux/cdev.h>
 
-MODULE_LICENSE("WTFPL");
-MODULE_AUTHOR("Dmitrii Medvedev");
-MODULE_DESCRIPTION("A simple example Linux module.");
-MODULE_VERSION("0.1");
+static dev_t first;
+static struct cdev c_dev; 
+static struct class *cl;
 
-static struct proc_dir_entry* entry;
-
-static ssize_t proc_write(struct file *file, const char __user * ubuf, size_t count, loff_t* ppos) 
+static int my_open(struct inode *i, struct file *f)
 {
-	printk(KERN_DEBUG "Attempt to write proc file");
-	return -1;
+  printk(KERN_INFO "Driver: open()\n");
+  return 0;
 }
 
-static ssize_t proc_read(struct file *file, char __user * ubuf, size_t count, loff_t* ppos) 
+static int my_close(struct inode *i, struct file *f)
 {
-	size_t len = strlen(THIS_MODULE->name);
-	if (*ppos > 0 || count < len)
-	{
-		return 0;
-	}
-	if (copy_to_user(ubuf, THIS_MODULE->name, len) != 0)
-	{
-		return -EFAULT;
-	}
-	*ppos = len;
-	return len;
+  printk(KERN_INFO "Driver: close()\n");
+  return 0;
 }
 
-static struct file_operations fops = {
-	.owner = THIS_MODULE,
-	.read = proc_read,
-	.write = proc_write,
+static ssize_t my_read(struct file *f, char __user *buf, size_t len, loff_t *off)
+{
+  printk(KERN_INFO "Driver: read()\n");
+  return 0;
+}
+
+static ssize_t my_write(struct file *f, const char __user *buf,  size_t len, loff_t *off)
+{
+  printk(KERN_INFO "Driver: write()\n");
+  return len;
+}
+
+static struct file_operations mychdev_fops =
+{
+  .owner = THIS_MODULE,
+  .open = my_open,
+  .release = my_close,
+  .read = my_read,
+  .write = my_write
 };
-
-
-static int __init proc_example_init(void)
+ 
+static int __init ch_drv_init(void)
 {
-	entry = proc_create("proc_example", 0444, NULL, &fops);
-	printk(KERN_INFO "%s: proc file is created\n", THIS_MODULE->name);
-	return 0;
+    printk(KERN_INFO "Hello!\n");
+    if (alloc_chrdev_region(&first, 0, 1, "ch_dev") < 0)
+	  {
+		return -1;
+	  }
+    if ((cl = class_create(THIS_MODULE, "chardrv")) == NULL)
+	  {
+		unregister_chrdev_region(first, 1);
+		return -1;
+	  }
+    if (device_create(cl, NULL, first, NULL, "mychdev") == NULL)
+	  {
+		class_destroy(cl);
+		unregister_chrdev_region(first, 1);
+		return -1;
+	  }
+    cdev_init(&c_dev, &mychdev_fops);
+    if (cdev_add(&c_dev, first, 1) == -1)
+	  {
+		device_destroy(cl, first);
+		class_destroy(cl);
+		unregister_chrdev_region(first, 1);
+		return -1;
+	  }
+    return 0;
 }
-
-static void __exit proc_example_exit(void)
+ 
+static void __exit ch_drv_exit(void)
 {
-	proc_remove(entry);
-	printk(KERN_INFO "%s: proc file is deleted\n", THIS_MODULE->name);
+    cdev_del(&c_dev);
+    device_destroy(cl, first);
+    class_destroy(cl);
+    unregister_chrdev_region(first, 1);
+    printk(KERN_INFO "Bye!!!\n");
 }
-
-module_init(proc_example_init);
-module_exit(proc_example_exit);
+ 
+module_init(ch_drv_init);
+module_exit(ch_drv_exit);
+ 
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Author");
+MODULE_DESCRIPTION("The first kernel module");
 
